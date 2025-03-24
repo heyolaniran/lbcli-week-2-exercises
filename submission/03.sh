503e9413a99a5271ffc18eb685f426fe2c6136f3
heyolaniran <olaniran.abd@gmail.com>
2025-03-24 02:53:31 +0100

    fix :  rpc wallet

--- submission/03.sh
+++ submission/03.sh
@@ -1,5 +1,5 @@
 # Created a SegWit address.
-SEGWIT_ADDR=$(bitcoin-cli -regtest getnewaddress "" "bech32")
+SEGWIT_ADDR=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" "bech32")
 # Add funds to the address.
 FUNDS=$(bitcoin-cli -regtest generatetoaddress 200 $SEGWIT_ADDR)
 # Return only the Address
