cc80bf463e7ff5c0896919c1fdadcec212e174df
heyolaniran <olaniran.abd@gmail.com>
2025-03-24 03:07:36 +0100

    fix: load wallet

--- submission/02.sh
+++ submission/02.sh
@@ -1,3 +1,2 @@
-# Create a new Bitcoin address, for receiving change.
-bitcoin-cli -regtest loadwallet btrustwallet
-echo bitcoin-cli -regtest getnewaddress "change"
+# Create a new Bitcoin address, for receiving chang
+bitcoin-cli -regtest getnewaddress
